518fc52ad4835966c8dc2292be1710fcd03936c0
julio2505 <juliomeza2510@outlook.com>
2024-01-08 21:58:01 -0600

    Added verify token route

--- src/controllers/whatsappController.js
+++ src/controllers/whatsappController.js
@@ -1,5 +1,21 @@
 const verifyToken = (req, res) => {
-    res.send("hola verifyToken");
+    
+    try {
+        var accessToken = "my_token";
+        var token = req.query["hub.verify_token"];
+        //lo que tenemos que devolver al verificar que los tokens son iguales
+        var challenge = req.body["hub.challenge"];
+
+        if(challenge != null && token != null && token === accessToken){
+            res.send(challenge);
+        } else {
+            res.status(400).send();
+        }
+
+    } catch(e){
+        res.status(400).send();
+    }
+
 }
 
 const receivedMessage = (req, res) => {
