dc77ea6caa5cd0833f0369a5348629db4afe63ef
julio2505 <juliomeza2510@outlook.com>
2024-01-08 22:01:48 -0600

    Fixed very token route

--- src/controllers/whatsappController.js
+++ src/controllers/whatsappController.js
@@ -4,7 +4,7 @@ const verifyToken = (req, res) => {
         var accessToken = "my_token";
         var token = req.query["hub.verify_token"];
         //lo que tenemos que devolver al verificar que los tokens son iguales
-        var challenge = req.body["hub.challenge"];
+        var challenge = req.query["hub.challenge"];
 
         if(challenge != null && token != null && token === accessToken){
             res.send(challenge);
